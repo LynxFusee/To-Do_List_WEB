897b5344ecd161c3588e7498f8cb504cec0999cc
Jules <jules.mangin@epita.fr>
2025-07-04 21:06:37 +0200

    tous les blocs principaux bien placés

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ app.get('/main', function (req, res){
     res.render('src/index')
 });
 
+app.get('/login', function (req, res){
+    res.render('src/login')
+});
+
 app.listen(8080);
 console.log("8080 pour le port bg")
